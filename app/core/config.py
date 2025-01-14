0379e003579c173001c738914a1a246d6234657b
Lucian Hanga <lucianhanga@gmx.net>
2025-01-14 15:33:50 +0100

    Update .gitignore to exclude application log files and modify DATABASE_URL to use localhost

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,14 @@
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://myuser:mypassword@db/mydb"
+    # DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = "postgresql+asyncpg://myuser:mypassword@localhost:5432/mydb"
 
 settings = Settings()
+
+# Print the DATABASE_URL to verify it's loaded correctly
+print(f"DATABASE_URL: {settings.DATABASE_URL}")
